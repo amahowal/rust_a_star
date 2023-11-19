24e589568bb336637d330063f93a34ecadef0fac
Austin Mahowald <austin.mahowald@gmail.com>
2023-11-19 14:19:06 -0800

    skeleton of plan solve

--- a_star/src/a_star/mod.rs
+++ a_star/src/a_star/mod.rs
@@ -27,13 +27,16 @@ impl Node {
     }
 }
 
-fn isGoal(activeNode: &Node, goalNode: &Node) {
+fn isGoal(activeNode: &Node, goalNode: &Node) -> bool {
+    true
 }
 
-fn generateSuccessorNodes(activeNode: &Node) {
+fn generateSuccessorNodes(activeNode: &Node) -> Node {
+    let mut originNode = Node { i: 0, j: 0, g: 0, h: 0, f: 0 };
+    originNode
 }
 
-pub fn aStarPlan(grid: &crate::occupancy_grid::OccuGrid) {
+pub fn aStarPlan(grid: &crate::occupancy_grid::OccuGrid) -> u32 {
     // initialize the goal node (rows and cols are 0-index)
     let goalNode = Node { i: grid.width-1, j: grid.length-1, g: 0, h: 0, f: 0 };
     dbg!(&goalNode.i);
@@ -43,14 +46,27 @@ pub fn aStarPlan(grid: &crate::occupancy_grid::OccuGrid) {
     // initialize the starting node, g=0, and calculate H, then add to the openNode set
     let mut originNode = Node { i: 0, j: 0, g: 0, h: 0, f: 0 };
     originNode.calc_h();
+    openNodes.push(originNode);
     // while the open node set is not empty
+    let mut activeNode: Node;
+    let mut counter: u32 = 0;
     while openNodes.len() > 0 {
-        // pick the parent off the open set with the lowest f value
-        //activeNode = openNodes.pop();
-        // check for goal
-        //isGoal(&activeNode, &goalNode);
-        // generate successor nodes
-        //openNode.append(generateSuccessorNodes(&activeNode));
-        openNodes.remove(0);
+        counter += 1;
+        // sort the open set from highest to lowest f value
+        dbg!(&openNodes[0]);
+        // pop the parent off the open set with the lowest f value
+        activeNode = openNodes.pop().unwrap();
+
+        // TODO: when to check if is goal?
+        if isGoal(&activeNode, &goalNode) {
+            // check for goal
+            return counter
+        } else {
+            // generate successor nodes
+            openNodes.push(generateSuccessorNodes(&activeNode));
+        }
     }
+    // TODO: how to return a number or a failure? return an error probably?
+    //       probably a result?
+    return 0
 }
